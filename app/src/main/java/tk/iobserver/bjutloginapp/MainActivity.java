785be980547f22bc63fa85929c3ad8e7bc2b4916
ZeroGoYoosee <qz777zp@live.cn>
2017-02-22 23:57:02 +0800

    Lots of Features Added
    Design UI
    Add Pemissions
    Add Sync Function
    Upgrade Version 2.1.0 Alpha

--- app/src/main/java/tk/iobserver/bjutloginapp/MainActivity.java
+++ app/src/main/java/tk/iobserver/bjutloginapp/MainActivity.java
@@ -1,32 +1,58 @@
 package tk.iobserver.bjutloginapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private SharedPreferences prefs;
+    final OkHttpClient okHttpClient = new OkHttpClient();
+    @BindView(R.id.main_toolbar) Toolbar toolbar;
+    @BindView(R.id.main_layout) CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.main_tv_usedFlux) TextView usedFlux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        fab.setOnClickListener(view -> {
+            refresh();
         });
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        refresh();
     }
 
     @Override
@@ -43,14 +69,60 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch (id){
+        switch (id) {
             case R.id.action_settings: {
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
             }
             break;
+            case R.id.action_login: {
+
+            }
+            break;
+            case R.id.action_logout: {
+
+            }
+            break;
+            case R.id.action_sync: {
+                refresh();
+            }
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    void refresh() {
+        Snackbar.make(coordinatorLayout, "Refresh data...", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        Request request = new Request.Builder()
+                .get()
+                .url("http://lgn.bjut.edu.cn/")
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Pattern pattern = Pattern.compile("flow='(\\d+)");
+                Matcher matcher = pattern.matcher(response.body().string());
+
+                if (matcher.find()) {
+                    final Double flux = (double) ((int) (Double.parseDouble(matcher.group(1)) / 1024 * 100)) / 100;
+                    runOnUiThread(() -> {
+                        try {
+                            usedFlux.setText("Used Flux: " + flux + "MB");
+                        } catch (Exception e) {
+                            Log.e(TAG, "", e);
+                        }
+                    });
+                }
+            }
+        });
+        Snackbar.make(coordinatorLayout, "Completed! ", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
 }
