17db2551a84f89f8bbaeb85a719a98855d0bf638
ZeroGoYoosee <qz777zp@live.cn>
2017-02-22 22:57:51 +0800

    Create Project & Refactor Code
    Create Project & Refactor Code
    Upgrade Version 2.0.0 Alpha

---
+++ app/src/main/java/tk/iobserver/bjutloginapp/SplashActivity.java
@@ -0,0 +1,37 @@
+package tk.iobserver.bjutloginapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import tk.iobserver.bjutloginapp.R;
+import tk.iobserver.bjutloginapp.BuildConfig;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 750;
+    @BindView(R.id.splash_tv_version) TextView version;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        version.setText(BuildConfig.VERSION_NAME);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
