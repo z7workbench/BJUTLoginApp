2f365a4e6edbf84d1b5c72a03d0abd9918eb5583
ZeroGoYoosee <qz777zp@live.cn>
2017-02-23 18:08:17 +0800

    Optimize Operator
    Optimize Operator
    Fix a problem with S-Chinese Edition
    Upgrade Version 2.2.5 Beta

--- app/src/main/java/tk/iobserver/bjutloginapp/Operator.java
+++ app/src/main/java/tk/iobserver/bjutloginapp/Operator.java
@@ -26,44 +26,43 @@ import okhttp3.Response;
 public class Operator {
     final OkHttpClient okHttpClient = new OkHttpClient();
     private String TAG;
-
-    public Operator(){
-        TAG = null;
-    }
+    private View view;
 
     public Operator(String TAG){
         this.TAG = TAG;
     }
 
     void login(View view, String user, String password){
-        RequestBody requestBody = new FormBody.Builder()
-                .add("DDDDD", user)
-                .add("upass", password)
-                .add("6MKKey", "123")
-                .build();
-        Request request = new Request.Builder()
-                .post(requestBody)
-                .url("http://wlgn.bjut.edu.cn/")
-                .build();
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Snackbar.make(view, "Login Failed! " + e, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.body().string().indexOf("In use")>0){
-                    Snackbar.make(view, "Login Failed! " + "This account is in use. ", Snackbar.LENGTH_LONG)
+        if(!user.isEmpty()){
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("DDDDD", user)
+                    .add("upass", password)
+                    .add("6MKKey", "123")
+                    .build();
+            Request request = new Request.Builder()
+                    .post(requestBody)
+                    .url("http://wlgn.bjut.edu.cn/")
+                    .build();
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Snackbar.make(view, "Login Failed! ", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                } else {
-                    Snackbar.make(view, "Login Succeeded! ", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    Log.e(TAG, "Failed! " , e);
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.body().string().indexOf("In use") > 0) {
+                        Snackbar.make(view, "Login Failed! " + "This account is in use. ", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    } else {
+                        Snackbar.make(view, "Login Succeeded! ", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                }
+            });
+        }
     }
 
     void refresh(View view, TextView textView, Activity activity) {
@@ -74,7 +73,9 @@ public class Operator {
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e(TAG, "", e);
+                Snackbar.make(view, "Refresh Failed! ", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Log.e(TAG, "Failed! ", e);
             }
 
             @Override
@@ -91,11 +92,14 @@ public class Operator {
                             Log.e(TAG, "", e);
                         }
                     });
+                    Snackbar.make(view, "Refresh successfully completed! ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(view, "Can't get data! ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
                 }
             }
         });
-        Snackbar.make(view, "Completed! ", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
     }
 
     void logout(View view){
@@ -106,8 +110,9 @@ public class Operator {
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Snackbar.make(view, "Logout Failed! " + e, Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Logout Failed! ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.e(TAG, "Failed!", e);
             }
 
             @Override
