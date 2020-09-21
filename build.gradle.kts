b5a89177818fd6491aed8702a7c472c757db92e1
ZeroGo Yoosee <qz777zp@live.cn>
2020-09-21 23:04:55 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha07")
-        classpath(kotlin("gradle-plugin", version = "1.4.0"))
+        classpath("com.android.tools.build:gradle:4.2.0-alpha11")
+        classpath(kotlin("gradle-plugin", version = "1.4.10"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
