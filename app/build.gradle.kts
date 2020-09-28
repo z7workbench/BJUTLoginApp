2a18bf5ac005d6f25f1b39861914a6f8b74f5e52
ZeroGo Yoosee <qz777zp@live.cn>
2020-09-28 18:49:22 +0800

    app 6.0.0-alpha7
    user item card elevation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdkVersion(26)
         targetSdkVersion(30)
         versionCode = 6
-        versionName = "6.0.0-alpha6"
+        versionName = "6.0.0-alpha7"
         versionNameSuffix = " (${gitCommitCount})"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
