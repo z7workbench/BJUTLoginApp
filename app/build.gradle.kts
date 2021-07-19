0dbba44f76a0f29bfcadeafd197f7515ce60ddba
ZeroGo Yoosee <qz777zp@live.cn>
2021-07-19 15:17:15 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.codehaus.groovy.runtime.ProcessGroovyMethods
 
-val lifecycleVersion = "2.4.0-alpha01"
+val lifecycleVersion = "2.4.0-alpha02"
 val navVersion = "2.3.5"
 val roomVersion = "2.4.0-alpha03"
 val permissionsdispatcher = "4.8.0"
@@ -53,8 +53,8 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib", "1.5.20"))
     // AndroidX
-    implementation("androidx.appcompat:appcompat:1.4.0-alpha02")
-    implementation("androidx.core:core-ktx:1.6.0-rc01")
+    implementation("androidx.appcompat:appcompat:1.4.0-alpha03")
+    implementation("androidx.core:core-ktx:1.7.0-alpha01")
     // Preference
     implementation("androidx.preference:preference:1.1.1")
     implementation("androidx.preference:preference-ktx:1.1.1")
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0-beta02")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("com.google.android.material:material:1.4.0-rc01")
+    implementation("com.google.android.material:material:1.4.0")
     // Activity
     implementation("androidx.activity:activity:$activityVersion")
     implementation("androidx.activity:activity-ktx:$activityVersion")
@@ -91,26 +91,26 @@ dependencies {
     // Feature module Support
     implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0-beta02")
+    implementation("androidx.datastore:datastore-preferences:1.0.0-rc01")
 
     // Jetpack Compose UI
-    implementation("androidx.compose.ui:ui:1.0.0-beta09")
+    implementation("androidx.compose.ui:ui:1.0.0-rc01")
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta09")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc01")
     // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-    implementation("androidx.compose.foundation:foundation:1.0.0-beta09")
+    implementation("androidx.compose.foundation:foundation:1.0.0-rc01")
     // Material Design
-    implementation("androidx.compose.material:material:1.0.0-beta09")
+    implementation("androidx.compose.material:material:1.0.0-rc01")
     // Material design icons
-    implementation("androidx.compose.material:material-icons-core:1.0.0-beta09")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta09")
+    implementation("androidx.compose.material:material-icons-core:1.0.0-rc01")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0-rc01")
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
     // Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
     // Integration with observables
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta09")
-    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-beta09")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-rc01")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-rc01")
 
     implementation("com.android.volley:volley:1.2.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
