f77bea1f88f12911d7523aec161a031020252c28
ZeroGo Yoosee <qz777zp@live.cn>
2021-10-21 10:55:26 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,13 +70,13 @@ dependencies {
     // AndroidX
     implementation("androidx.appcompat:appcompat:1.4.0-alpha03")
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.core:core-splashscreen:1.0.0-alpha01")
+    implementation("androidx.core:core-splashscreen:1.0.0-alpha02")
     // Preference
     implementation("androidx.preference:preference:1.1.1")
     implementation("androidx.preference:preference-ktx:1.1.1")
     // Layouts
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("com.google.android.material:material:1.4.0")
     // Activity
@@ -100,8 +100,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose"]}")
     implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose"]}")
     implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("com.google.android.material:compose-theme-adapter:1.0.2")
-    implementation("com.google.accompanist:accompanist-appcompat-theme:0.17.0")
+    implementation("com.google.android.material:compose-theme-adapter:1.0.4")
+    implementation("com.google.accompanist:accompanist-appcompat-theme:0.20.0")
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     // Paging
